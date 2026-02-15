69f103ea7eaa409fbbd20b9df655bbfcbbee647c
brettdunsmore <dunsmore.brett@gmail.com>
2026-02-15 12:01:36 +1000

    Update ReferralInterface.tsx

--- src/components/ReferralInterface.tsx
+++ src/components/ReferralInterface.tsx
@@ -142,7 +142,7 @@ export const ReferralInterface: React.FC = () => {
               </a>
             </Button>
             <p className="text-[10px] text-zinc-500 max-w-[280px] mx-auto leading-relaxed font-medium">
-              By using this referral code/link and obtaining a Game Package with a minimum value of $40 USD on the Pledge Store, you will receive <span className="text-amber-500/90 font-bold">50,000 UEC</span> starting credits and a potential bonus seasonal reward (if applicable at time of use).
+              By using this referral code/link and obtaining a Game Package with a minimum value of $40 USD on the Pledge Store, you will receive <span className="text-amber-500/90 font-bold">50,000 UEC</span> starting credits and a potential bonus seasonal reward (subject to applicability at time of use).
             </p>
           </div>
         </div>
