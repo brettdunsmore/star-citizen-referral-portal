66ed6d48a50ac5c863bccdb9b2aa3c7c06f028ac
brettdunsmore <dunsmore.brett@gmail.com>
2026-02-15 12:19:45 +1000

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -77,6 +77,13 @@ export function HomePage() {
                 />
               </div>
             </motion.div>
+            
+                    <div className="text-[9px] uppercase tracking-[0.3em] text-zinc-600 font-medium">
+  Referred by{' '}
+  <a href="https://robertsspaceindustries.com/en/citizens/Quantum_Reaction" target="_blank" rel="noopener noreferrer" className="text-amber-400 hover:text-amber-300 underline">
+    Quantum_Reaction
+  </a>
+</div>
             <motion.footer
               variants={itemVariants}
               className="mt-12 text-[8px] uppercase tracking-[0.5em] text-zinc-700 font-medium px-6 max-w-[90vw] mx-auto select-none"
@@ -84,15 +91,7 @@ export function HomePage() {
               Star Citizen is a registered trademark of Cloud Imperium Games.
             </motion.footer>
           </motion.div>      
-        </main>
-          
-        <div className="text-[9px] uppercase tracking-[0.3em] text-zinc-600 font-medium">
-  Referred by{' '}
-  <a href="https://robertsspaceindustries.com/en/citizens/Quantum_Reaction" target="_blank" rel="noopener noreferrer" className="text-amber-400 hover:text-amber-300 underline">
-    Quantum_Reaction
-  </a>
-</div>
-        
+        </main>        
       </div>
       <Toaster
         position="bottom-center"
